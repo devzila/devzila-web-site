64d7470eee4c8e102070dc8a51bfded0eabc10f7
meenakshi-devzila <meenakshi@devzila.com>
2019-09-16 13:52:43 +0530

    projects page

--- projects.php
+++ projects.php
@@ -2,121 +2,125 @@
 <html lang="zxx">
 	<head>
 		<title>Devzila Software Solutions</title>
-			<?php include("inc/head.inc.php");?>
-				<style>
-					.overlay-div:hover{
-						max-width: 100%;
-						max-height: 100%;
-						background: grey;	
-						overflow: hidden;
-					}
-				    .cursor:hover {
-						opacity: .7;
-						transition: 1s;
-						-ms-transform: scale(1.5); /* IE 9 */
-						-webkit-transform: scale(1.5); /* Safari 3-8 */
-						transform: scale(1.2); 
-					}
-	            </style>
+		<?php include("inc/head.inc.php");?>
 	</head>
     <body>
-   	<?php include("inc/header.inc.php");?>
-   	<div class="section-block-grey">
-         <div class="container">
-            <div class="section-heading center-holder">
-            
-               <h3>Projects</h3>
-
-            <div class="row mt-50">
-
-               <div class="col-md-4 col-sm-4 col-12">
-               		<div class="overlay-div">
+   		<?php include("inc/header.inc.php");?>
+   		<div class="section-block-grey">
+         	<div class="container">
+           	 	<div class="section-heading center-holder">
+            		<h3>Projects</h3>
+            		<div class="row mt-50">
+               	<div class="col-md-4 col-sm-4 col-12">
+               		<div class="overlay-div hvrbox">
                   		<div class="blog-grid cursor">
                      		<div class="blog-grid-img">
-                        		<img src="img/img_6.jpg" alt="img" >
+                        		<img src="img/img_6.jpg" alt="img" class="hvrbox-layer_bottom">
                      		</div>
                      		<div class="blog-grid-text">
+                     			<div class="hvrbox-layer_top">
+                     			<p>NGTU</p>
+                     			<h2 class="hvrbox-text">Not going to uni</h2>
+                     			</div>
                         		<h4 style="text-align: center;">NGtU</h4>
                         		<p>Not Going to University is an open platform for students and job seekers. </p>
                      		</div>
                   		</div>
               		</div>
                </div>
-
                <div class="col-md-4 col-sm-4 col-12">
-               	    <div class="overlay-div">
+               	    <div class="overlay-div hvrbox">
                   		<div class="blog-grid cursor">
                      		<div class="blog-grid-img">
-                       			 <img src="img/img_2.jpg" alt="img">
+                       			 <img src="img/img_2.jpg" alt="img" class="hvrbox-layer_bottom">
                        		</div>
                      		<div class="blog-grid-text">
+                     			<div class="hvrbox-layer_top">
+                     				<p>Coverwallet</p>
+                     			<h2 class="hvrbox-text">New York website</h2>
+                     			</div>
                         		<h4 style="text-align:center;">Coverwallet</h4>
                         		<p>CoverWallet is a New York based website that offers insurance for businesses.</p>
                         	</div>
                         </div>
               		</div>
                </div>
-
                <div class="col-md-4 col-sm-4 col-12">
-               		<div class="overlay-div">
+               		<div class="overlay-div hvrbox">
                   		<div class="blog-grid cursor">
                     		<div class="blog-grid-img">
-                        		<img src="img/img_1.jpg" alt="img">
+                        		<img src="img/img_1.jpg" alt="img" class="hvrbox-layer_bottom">
                     		</div>
-                     	<div class="blog-grid-text">
-                        	<h4 style="text-align: center;">Vreasy</h4>
-                        	<p>Rental apartment management app for property manager allows auto management tools.</p>
-                     	</div>
+                     		<div class="blog-grid-text">
+                     			<div class="hvrbox-layer_top">
+                     				<p>Vreasy</p>
+                     				<h2 class="hvrbox-text">Rental Apartment Management </h2>
+                     			</div>
+                        			<h4 style="text-align: center;">Vreasy</h4>
+                        			<p>Rental apartment management app for property manager allows auto management tools.</p>
+                     		</div>
                   		</div>
-               </div>
+               		</div>
                </div>
             </div>
             <div class="row mt-50">
-               <div class="col-md-4 col-sm-4 col-12">
-               	<div class="overlay-div">
-                  <div class="blog-grid cursor">
-                     <div class="blog-grid-img">
-                        <img src="img/img_3.jpg" alt="img">
-                     </div>
-                     <div class="blog-grid-text">
-                        <h4 style="text-align: center;">Turnstr</h4>
-                        <p>Turnstr is the best way to customize your photos and videos with custom filters and frames.</p>
-                     </div>
-                  </div>
-              </div>
+               	<div class="col-md-4 col-sm-4 col-12">
+               		<div class="overlay-div hvrbox">
+                  		<div class="blog-grid cursor">
+                     		<div class="blog-grid-img">
+                        		<img src="img/img_3.jpg" alt="img" class="hvrbox-layer_bottom">
+                     		</div>
+                     		<div class="blog-grid-text">
+                     			<div class="hvrbox-layer_top">
+                     				<p>Turnstr</p>
+                     				<h2 class="hvrbox-text">Customize Photos</h2>
+                     			</div>
+                        		<h4 style="text-align: center;">Turnstr</h4>
+                        		<p>Turnstr is the best way to customize your photos and videos with custom filters and frames.</p>
+                     		</div>
+                  		</div>
+              		</div>
                </div>
             <div class="col-md-4 col-sm-4 col-12">
-               	<div class="overlay-div">
-                  <div class="blog-grid cursor">
-                     <div class="blog-grid-img">
-                        <img src="img/img_4.jpg" alt="img">
-                     </div>
-                     <div class="blog-grid-text">
-                        <h4 style="text-align:center;">SnapHubr</h4>
-                        <p>Snaphubr is an African high stock image website and marketplace where people simply upload and sell their images.</p>
-                     </div>
-                  </div>
-              </div>
-               </div>
-
+               	<div class="overlay-div hvrbox">
+                  	<div class="blog-grid cursor">
+                     	<div class="blog-grid-img">
+                        	<img src="img/img_4.jpg" alt="img" class="hvrbox-layer_bottom">
+                     	</div>
+                     	<div class="blog-grid-text">
+                     		<div class="hvrbox-layer_top">
+                     			<p>SnapHubr</p>
+                     			<h2 class="hvrbox-text">High Stock Image Website</h2>
+                     		</div>
+                        		<h4 style="text-align:center;">SnapHubr</h4>
+                        		<p>Snaphubr is an African high stock image website and marketplace where people simply upload and sell their images.</p>
+                     	</div>
+                  	</div>
+              	</div>
+            </div>
                <div class="col-md-4 col-sm-4 col-12">
-               	<div class="overlay-div">
-                  <div class="blog-grid cursor">
-                     <div class="blog-grid-img">
-                        <img src="img/img_5.jpg" alt="img">
-                     </div>
-                     <div class="blog-grid-text">
-                        <h4 style="text-align: center;">Pixable</h4>
-                        <p>Photo aggregator/inbox.The photo app lets user see the top photos and videos shared by your friends on Facebook.</p>
-                     </div>
-                  </div>
-               </div>
+               		<div class="overlay-div hvrbox">
+                  		<div class="blog-grid cursor">
+                     		<div class="blog-grid-img">
+                        		<img src="img/img_5.jpg" alt="img" class="hvrbox-layer_bottom">
+                    		</div>
+                     		<div class="blog-grid-text">
+                     			<div class="hvrbox-layer_top">
+                     				<p>Pixable</p>
+                     			<h2 class="hvrbox-text">Photo aggregator/inbox</h2>
+                     			</div>
+                        		<h4 style="text-align: center;">Pixable</h4>
+                        		<p>Photo aggregator/inbox.The photo app lets user see 	the top photos and videos shared by your friends on 	Facebook.</p>
+                     		</div>
+                  		</div>
+              		</div>
                </div>
             </div>
-         </div>
-      </div>
-      <div class="notice-section notice-section-sm border-top">
-         <div class="container">
+        </div>
+    </div>
+</div>
+    <div class="notice-section notice-section-sm border-top">
+        <div class="container">
             <div class="row">
                <div class="col-md-8 col-sm-8 col-12">
                   <div class="mt-5 ml-5">
@@ -128,7 +132,8 @@
                   <div class="mt-5 mb-5 right-holder-lg"><a href="contact-us" class="primary-button button-md mt-15-xs">Contact Us Today</a></div>
                </div>
             </div>
-         </div>
+        </div>
+    </div>
    	<?php include("inc/footer.inc.php");?>
    </body>
 </html>
